27d306ece3be5d5109733bf8521d367bf728c7ff
salvi9800 <52711569+salvi9800@users.noreply.github.com>
2021-11-21 05:36:32 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt import views as jwt_views
 from . import views
 urlpatterns = [
     path('register/', views.UserRegistrationView.as_view()),
-    path('token', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'), #Obtain both Access & refresh tokens
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'), #Obtain both Access & refresh tokens
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'), #returns new refresh token and blacklist the previos refresh token 
     path('login/', views.UserLoginView.as_view()),
     path('logout/', views.BlacklistRefreshView.as_view()),
